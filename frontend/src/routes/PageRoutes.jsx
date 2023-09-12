87988aa1db184330afafe4f21aedf81a9f72bd54
Sharpy776 <bswjleon001@mymail.sim.edu.sg>
2023-09-13 00:07:50 +0800

    Added the UserSupportPage and updated PageRoutes index.html tab name changed to AniFace Webapp

--- frontend/src/routes/PageRoutes.jsx
+++ frontend/src/routes/PageRoutes.jsx
@@ -1,10 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import UserHome from "../pages/User/UserHome";
+import UserSupportPage from "../pages/User/UserSupportPage";
 
 function PageRoutes() {
     return (
         <Routes>
             <Route path="/UserHome" element={<UserHome />} />
+            <Route path="/UserSupportPage" element={<UserSupportPage />} />
         </Routes>
     );
 }
