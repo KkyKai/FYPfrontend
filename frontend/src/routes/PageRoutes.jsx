61194ad6a5d01fccd40b772339091f80846ecdb5
Sharpy776 <bswjleon001@mymail.sim.edu.sg>
2023-09-13 18:21:40 +0800

    Created UserSubscriptionPlans page for users to check plans and SubscriptionInfoBadge to provide sub info and button
    Updated routes
    
    UserNavbar margintop set to -75 to align to top of page (pages with color or images would wrap above navbar)

--- frontend/src/routes/PageRoutes.jsx
+++ frontend/src/routes/PageRoutes.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import UserHome from "../pages/User/UserHome";
 import UserSupportPage from "../pages/User/UserSupportPage";
+import UserSubscriptionPlans from "../pages/User/UserSubscriptionPlans";
 
 function PageRoutes() {
     return (
         <Routes>
             <Route path="/UserHome" element={<UserHome />} />
             <Route path="/UserSupportPage" element={<UserSupportPage />} />
+            <Route path="/UserSubscriptionPlans" element={<UserSubscriptionPlans />} />
         </Routes>
     );
 }
