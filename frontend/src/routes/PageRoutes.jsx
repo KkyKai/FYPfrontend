2eedee07cdd9ce47c32df9d242672ba561c14e34
lawneng <95595420+lawneng@users.noreply.github.com>
2023-09-14 13:36:10 +0800

    Set the main page to homepage, remove unneccessary packets.

--- frontend/src/routes/PageRoutes.jsx
+++ frontend/src/routes/PageRoutes.jsx
@@ -9,12 +9,14 @@ import UserSubscriptionPlans from "../pages/User/UserSubscriptionPlans";
 function PageRoutes() {
     return (
         <Routes>
+            <Route path="/" element={<UserHome />} />
             <Route path="/UserHome" element={<UserHome />} />
             <Route path="/UserSupportPage" element={<UserSupportPage />} />
             <Route path="/UserFAQPage" element={<UserFAQPage />} />
             <Route path="/UserSettingPage" element={<UserSettingPage />} />
             <Route path="/Testing" element={<Testing />} /> 
             <Route path="/UserSubscriptionPlans" element={<UserSubscriptionPlans />} />
+
         </Routes>
     );
 }
