481625c1cdd237a4bcd994559b3fa65438417e3d
lawneng <95595420+lawneng@users.noreply.github.com>
2023-09-16 11:01:28 +0800

    Image upload fixed

--- frontend/src/pages/User/Testing.jsx
+++ frontend/src/pages/User/Testing.jsx
@@ -1,43 +1,77 @@
-import { useDisclosure } from '@mantine/hooks';
-import { Modal, Button, Group } from '@mantine/core';
 import { useState } from 'react';
-import {
-    createStyles,
-    Container,
-    Avatar,
-    UnstyledButton,
-    Text,
-    Menu,
-    Tabs,
-    Burger,
-    rem,
-} from '@mantine/core';
-import {
-    IconLogout,
-    IconHeart,
-    IconStar,
-    IconMessage,
-    IconSettings,
-    IconPlayerPause,
-    IconTrash,
-    IconSwitchHorizontal,
-    IconChevronDown,
-} from '@tabler/icons-react';
-import { MantineLogo } from '@mantine/ds';
-import { Link } from 'react-router-dom';
+import { Image, FileButton, Button, Group, Text, Space, Stack } from '@mantine/core';
 
 function Testing() {
-  // Original code
-const [tabOpened, { toggle: toggleTab }] = useDisclosure(false);
-const [userMenuOpened, setUserMenuOpened] = useState(false);
+    const [file, setFile] = useState(null | File);
+    const [cartoonizedSrc, setCartoonizedSrc] = useState(null);
+    //const [resetRef] = useState(null);
 
-// Updated code with the second useDisclosure
-const [modalOpened, { open: openModal, close: closeModal }] = useDisclosure(false);
+    const clearFile = () => {
+        setFile(null);
+        setCartoonizedSrc(null);
+    };
 
-return (
-    <>
-    </>
-);
+    const cartoonizeImage = () => {
+        if (file) {
+            setCartoonizedSrc("https://images.unsplash.com/photo-1682695794816-7b9da18ed470?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80");
+        }
+    }
+
+    // const handleImageUpload = (event) => {
+    //     if (event && event.target) {
+    //         const selectedFile = event.target.files[0];
+    //         setFile(URL.createObjectURL(selectedFile));
+    //         console.log(file);
+    //     }
+    // }
+
+    return (
+        <div>
+            <Stack align="center">
+                <Group>
+                    {file ? (
+                        <Image width={400} height={320} src={URL.createObjectURL(file)} alt="Original image" withPlaceholder />
+                    ) : (
+                        <Image width={400} height={320} src={null} alt="Original image" withPlaceholder />
+                    )}
+                    {cartoonizedSrc ? (
+                        <Image width={400} height={320} src={cartoonizedSrc} alt="Modified image" withPlaceholder />
+                    ) : (
+                        <Image width={400} height={320} src={null} alt="Modified image" withPlaceholder />
+                    )}
+                </Group>
+                <Space h="xl" />
+
+                <Group position="center">
+                    {/* <Image width={400} height={320} src={file} alt="Original image" withPlaceholder /> */}
+
+                    <div>
+                        {file ? (
+                            <Button onClick={cartoonizeImage}>
+                                Cartoonize
+                            </Button>
+                        ) : (
+                            <FileButton onChange={setFile} accept="image/png,image/jpeg">
+                                {(props) => <Button {...props} >Upload image</Button>}
+                            </FileButton>
+                        )}
+                    </div>
+
+                    
+                   
+                    <Button disabled={!file} color="red" onClick={clearFile}>
+                        Reset
+                    </Button>
+                </Group>
+            </Stack>
+
+            {file && (
+                <Text size="sm" align="center" mt="sm">
+                    Picked file: {file.name}
+                </Text>
+            )}
+        </div>
+    );
 }
 
 export default Testing;
